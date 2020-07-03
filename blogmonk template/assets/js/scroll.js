c545901b16076dba280631176c8f537ad30c758e
Dhananjaya Dutt Mishra <65168982+DDMX20@users.noreply.github.com>
2020-07-03 20:39:43 +0530

    Add files via upload

---
+++ blogmonk template/assets/js/scroll.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+$('.nav-link').click(function(e){
+    var linkHref = $(this).attr('href');
+    $('html, body').animate({
+            scrollTop: $(linkHref).offset().top
+          }, 1000);
+   e.preventDefault();
+});
+
+
+});
